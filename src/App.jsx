5847629c7275771b02bbeff7a071eb0cb1d9680b
Fernando Oliveros <foliverosocampo0322@gmail.com>
2025-05-15 16:16:57 -0500

    fix: cambiado nombre MisionVision

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Footer from './components/Footer/Footer'
 {/*Pages routes*/}
 import Inicio from './pages/Inicio/Inicio';
 import QuienesSomos from './pages/quienesSomos/quienesSomos';
-import MYV from './pages/M_Y_V/MYV.JSX';
+import MisionVision from './pages/M_Y_V/MisionVision';
 import Location from './pages/Location/Location';
 import Contact from './pages/Contact/Contact';
 function App() {
@@ -24,7 +24,7 @@ function App() {
           <Route path='/' element={<Inicio/>}/>
           <Route path='/sede' element={<Location/>}/>
           <Route path="/quienes-somos" element={<QuienesSomos />} />
-          <Route path="/mision-vision" element={<MYV />} />
+          <Route path="/mision-vision" element={<MisionVision />} />
           <Route path="/contacto" element={<Contact />} />
 
         </Routes>
